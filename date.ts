629b3b9128a99b971cfea3b462abf792aa7dbde3
mingjue8888 <mingjue.liu@icloud.com>
2024-08-30 01:53:29 +0800

    first commit

---
+++ date.ts
@@ -0,0 +1,16 @@
+import joi from "joi";
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+import quarterOfYear from "dayjs/plugin/quarterOfYear";
+import weekOfYear from "dayjs/plugin/weekOfYear";
+
+const NODE_TIMEZONE = joi.attempt(process.env.NODE_TIMEZONE, joi.string().default("Asia/Hong_Kong"));
+
+dayjs.extend(timezone);
+dayjs.extend(utc);
+dayjs.extend(quarterOfYear);
+dayjs.extend(weekOfYear);
+dayjs.tz.setDefault(NODE_TIMEZONE);
+
+export const date = dayjs;
